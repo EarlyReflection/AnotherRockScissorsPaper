f0feb0da1667be5203e6162982227de37ea4735c
Vladimir Dvornikov <earlyreflections@protonmail.com>
2023-08-07 20:21:58 +0300

    Initial Commit

---
+++ AnotherRockScissorsPaper/AnotherRockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherRockScissorsPaperApp.swift
+//  AnotherRockScissorsPaper
+//
+//  Created by Vladimir Dvornikov on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnotherRockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
